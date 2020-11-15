18a016db43166f7aba4846f68211f2c88e3155fe
Athanasios Taprantzis <thatapras@hotmail.gr>
2020-11-15 02:33:31 +0200

    First commit!

---
+++ qt.py
@@ -0,0 +1,82 @@
+import sys
+from quickstart import ret_lists,ret_tasks,func,delete
+from functools import partial
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget,\
+    QPushButton, QHBoxLayout, QGridLayout, QComboBox, QVBoxLayout, QScrollArea
+def selectionchange(all_tasks,lists,service,i):
+    # print(type(all_tasks),type(lists),type(i))
+    update_tasks(all_tasks,lists[i][1],service)
+    # print(all_tasks.count())
+
+def update_tasks(all_tasks,list_id,service):
+    tasks=ret_tasks(list_id,service)
+    for i in reversed(range(1,all_tasks.count())):
+        all_tasks.itemAt(i).widget().setParent(None)
+
+    for cnt,i in enumerate(tasks):
+        all_tasks.addWidget(QLabel(i[0]),cnt+1,0)
+        x=QPushButton("Button2")
+        y=QPushButton("Button2")
+        all_tasks.addWidget(x,cnt+1,1)
+        all_tasks.addWidget(y,cnt+1,2)
+        x.clicked.connect(partial(delete_button,all_tasks,list_id,tasks[cnt][1],service))
+
+def delete_button(all_tasks,list_id,task_id,service):
+
+    delete(all_tasks,list_id,task_id,service)
+    update_tasks(all_tasks,list_id,service)
+
+def main():
+    app=QApplication(sys.argv)
+    scroll = QScrollArea()
+    all_tasks=QGridLayout()
+    outer_window=QMainWindow()
+    outer_window.setCentralWidget(scroll)
+    window=QWidget()
+    scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+    scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+    scroll.setWidgetResizable(True)
+    scroll.setWidget(window)
+
+    service=func()
+    lists=ret_lists(service)
+    tasks=ret_tasks(lists[0][1],service)
+    window.setWindowTitle("My app")
+    window.setGeometry(100,100,280,80)
+    window.move(0,0)
+    drop_list=QComboBox()
+    all_tasks.addWidget(drop_list,0,0)
+
+    for i in lists:
+        drop_list.addItem(i[0])
+
+    for cnt,i in enumerate(tasks):
+        all_tasks.addWidget(QLabel(i[0]),cnt+1,0)
+        x=QPushButton("Button2")
+        y=QPushButton("Button2")
+        all_tasks.addWidget(x,cnt+1,1)
+        all_tasks.addWidget(y,cnt+1,2)
+        x.clicked.connect(partial(delete_button,all_tasks,lists[0][1],tasks[cnt][1],service))
+
+    print(all_tasks.count())
+    drop_list.currentIndexChanged.connect(partial(selectionchange,all_tasks,lists,service))
+    # all_tasks.change
+    # helloMsg=QLabel('<h1>Hello world!</h1>',parent=window)
+    # helloMsg.move(60,15)
+
+    # layout=QGridLayout()
+    # layout.addWidget(QPushButton('Button (0, 0)'), 0, 0)
+    # layout.addWidget(QPushButton('Button (0, 1)'), 0, 1)
+    # x=QPushButton('Button (0, 2)')
+    # layout.addWidget(x, 0, 2)
+    # x.clicked.connect(hey)
+
+    # layout.move(80,40)
+    window.setLayout(all_tasks)
+    outer_window.show()
+    sys.exit(app.exec_())
+
+
+if __name__=='__main__':
+    main()
---
+++ quickstart.py
@@ -0,0 +1,82 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import requests as r
+from pprint import pprint
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/tasks']
+name='https://www.googleapis.com//tasks/v1/users/@me/lists/'
+def func():
+    """Shows basic usage of the Tasks API.
+    Prints the title and ID of the first 10 task lists.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('tasks', 'v1', credentials=creds)
+    return service
+
+def sort_func(e):
+    return int(e['position'])
+def ret_lists(service):
+
+    # Call the Tasks API
+    results = service.tasklists().list().execute()
+    items = results['items']
+    # more=service.tasks().list(tasklist=items[1]['id']).execute()
+    final=[]
+    for i in items:
+        final.append((i['title'],i['id']))
+        # pprint(i)
+    return final
+    # pprint(final)
+    # pprint(items)
+
+def delete(all_tasks,list_id,task_id,service):
+    # print(drop_list.currentIndex())
+    # print(lists[0])
+    # print(tasks[:10])
+    # print(tasks[row])
+    # print(lists[drop_list.currentIndex()][1])
+    # print(tasks[row][1])
+    # return
+    # service.tasks().delete(tasklist=lists[drop_list.currentIndex()][1],task=tasks[row][1]).execute()
+    service.tasks().delete(tasklist=list_id,task=task_id).execute()
+
+
+def ret_tasks(target_list,service):
+    # results = service.tasklists().list().execute()
+    # items = results['items']
+    more=service.tasks().list(tasklist=target_list,maxResults=40,showCompleted=False,showHidden=True).execute()
+    # pprint(more)
+    try:
+        more=more['items']
+        more.sort(key=sort_func)
+        final=[]
+        for i in more:
+            final.append((i['title'],i['id']))
+        return final
+    except:
+        return [('Empty list - Add something','0')]
+if __name__ == '__main__':
+    ret_tasks('MDM4NDkxMDU0Mjk5OTcwMzU2NDM6MDow')
+    # ret_lists()
